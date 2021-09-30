ce143c556fd8b62ef4d1d8eaf5985864862519aa
3342-collab <juancitogimenez0@gmail.com>
2021-09-30 10:12:04 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,12 @@ int main(void)
   long int longinc;
   long long int longintinc;
   float floatinc;
+  
   // sizeof evaluates the size of a variable
   printf("Size of a char: %zu byte(s)\n", sizeof(charinc));
   printf("Size of an int: %zu byte(s)\n", sizeof(intinc));
   printf("Size of a long int: %zu byte(s)\n", sizeof(longinc));
   printf("Size of a long long int: %zu byte(s)\n", sizeof(longintinc));
-  printf("Size of a float: %zu byte(s)\n", sizeof((floatinc));
+  printf("Size of a float: %zu byte(s)\n", sizeof(floatinc));
 return (0);
 }
