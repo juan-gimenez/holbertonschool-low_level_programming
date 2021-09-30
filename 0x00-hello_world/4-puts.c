276ab1a51421b5796259d4ef10478825595e94ee
3342-collab <juancitogimenez0@gmail.com>
2021-09-30 08:03:43 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-/* 4-Hello, puts
-* Write a C program 
-*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-  {
-    puts(""Programming is like building a multilingual puzzle \n ");
-    return (0)
-      }
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
