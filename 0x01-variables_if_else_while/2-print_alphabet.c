ef0410b973f359760ea42c2b2251b8c0c875ecda
3342-collab <juancitogimenez0@gmail.com>
2021-10-01 10:59:35 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point progr
+ * Return: always zero
+ */
+int main(void)
+{
+char ch;
+for(ch=97; ch<= 122; ch++);
+putchar(ch);
+putchar('\n');
+return (0);
+}
