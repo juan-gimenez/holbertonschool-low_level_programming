51b3c9bc6ab9a7f882da547b4f4301fa9ac3d8cb
3342-collab <juancitogimenez0@gmail.com>
2021-10-02 14:31:26 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,10 @@
  */
 int main(void)
 {
-int num[120] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int num[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 int i;
 for (i = 0; i < 11; i++)
-if (i < 10)
 putchar(num[i]);
-else
 putchar('\n');
 return (0);
 }
