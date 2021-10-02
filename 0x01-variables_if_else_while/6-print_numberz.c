74ccb6702a49f1d2b7312ea4157789c5948ee329
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-02 16:30:13 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,9 @@
  */
 int main(void)
 {
-int num[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 int i;
-for (i = 0; i < 11; i++)
-putchar(num[i]);
+for (i = 48; i < 58; i++)
+putchar(i);
 putchar('\n');
 return (0);
 }
