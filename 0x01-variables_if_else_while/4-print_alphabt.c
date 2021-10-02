03931eb7cb1bf0614333146a0f3bb0ba38356a6c
3342-collab <juancitogimenez0@gmail.com>
2021-10-01 18:28:00 -0700

    task 4 finally done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,12 @@ int main(void)
 char abc[30] = "abcdefghijklmnopqrstuvwxyz";
 int i;
 for (i = 0; i < 26; i++)
+{
 if (i != 5 && i != 17)
+{
 putchar(abc[i]);
+}
+}
 putchar('\n');
 return (0);
 }
