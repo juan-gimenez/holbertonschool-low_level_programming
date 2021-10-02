b3ef0a051d80dc779c7756269238fa27897f5c3f
3342-collab <juancitogimenez0@gmail.com>
2021-10-01 19:55:48 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 11; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
