ac9f8aeb7b8a4136dc852031f9ca7af3f87f7b73
3342-collab <juancitogimenez0@gmail.com>
2021-10-01 17:02:48 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point progr
+ * Return: always zero
+ */
+int main(void)
+{
+char abc[30] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+if (i != 5 && i != 17)
+putchar(abc[i]);
+else if (NULL)
+return 0;
+}
