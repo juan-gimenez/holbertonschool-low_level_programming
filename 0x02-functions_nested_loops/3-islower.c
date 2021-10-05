3a4ca2a96ced60fdb311837fb3b1ec7ed2991d53
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-05 10:39:10 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,13 +6,8 @@
  */
 int _islower(int c)
 {
-  if (c >= 97 && c <= 122)
-    {
-      _putchar(49);
-    }
-  else
-    {
-      _putchar(48);
-    }
-  return (0);
+do { _putchar(49);
+}
+while (c >= 97 && c <= 122);
+return (0);
 }
