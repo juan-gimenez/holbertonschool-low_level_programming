4ba61ab7dda8a3c8dd44c5e9a3605985c9f4b5dc
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-05 10:17:20 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - if  c is lowercase return 1 if not 0
+ * Return: void
+ * @c: integer
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+_putchar(49);
+}
+else
+{
+_putchar(48);
+}
+return (0);
+}
