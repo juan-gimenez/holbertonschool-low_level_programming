7e77d785ec7337abcc4cad219fb2f11d761d44eb
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-05 07:02:06 -0700

    tasks 0x02

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -program that prints _putchar, followed by a new line.
+ * Return: 0 if success
+ */
+int main(void)
+{
+char str[] = "_putchar";
+printf("%s\n", str);
+return (0);
+}
