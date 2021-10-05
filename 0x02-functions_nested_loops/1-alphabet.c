f1234d84dc06af62fcc99937c44b106d2ef931d1
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-05 07:30:12 -0700

    tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+  int i;
+  for (i = 97; i <= 123; i++)
+    {
+      _putchar(i);
+      _putchar(i);
+    }
+}
