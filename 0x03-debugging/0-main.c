3f3d729c3a5244658f5a86eafbb3aca7d375a712
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-06 16:13:52 -0700

    task 0 and main.h fixed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,14 +3,13 @@
 /**
  * main - tests function that prints if a number is positive or negative
  * Return: 0
+ * @posorneg: value to consider
  */
 
 int main(void)
 {
 int posorneg;
-
 posorneg = 0;
 positive_or_negative(i);
-
 return (0);
 }
