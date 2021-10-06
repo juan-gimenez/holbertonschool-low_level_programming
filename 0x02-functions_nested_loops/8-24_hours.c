2b5561766e8cc97238779293faa852d038322d45
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-05 19:32:57 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - print the minutes of a random day
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+int hr1, hr2, min1, min2;
+
+for (hr1 = 48; hr1 <= 50; hr1++)
+{
+for (hr2 = 48; hr2 <= 57; hr2++)
+{
+for (min1 = 48; min1 <= 53; min1++)
+{
+for (min2 = 48; min2 <= 57; min2++)
+{
+if (hr1 >= 50 && hr2 >= 52)
+break;
+_putchar(hr1);
+_putchar(hr2);
+_putchar(58);
+_putchar(min1);
+_putchar(min2);
+_putchar('\n');
+}
+}
+}
+}
+}
