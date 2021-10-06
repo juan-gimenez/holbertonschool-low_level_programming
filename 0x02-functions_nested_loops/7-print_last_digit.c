106b01e91715ebde09e00c6ff75b5d324ede336f
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-05 17:52:58 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,14 @@ int print_last_digit(int c)
 {
 int digitlast;
 digitlast = c % 10;
-if (digitlast < 0 && digitlast > 0)
+if (digitlast < 0)
 {
-_putchar(digitlast);
+_putchar(-digitlast + 48);
+return (-digitlast);
 }
 else
 {
-_putchar(digitlast);
-}
+_putchar(digitlast + 48);
 return (digitlast);
+}
 }
