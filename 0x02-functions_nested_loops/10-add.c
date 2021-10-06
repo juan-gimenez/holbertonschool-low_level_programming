ced7aa60be0e8bda712e9494f58bf81c7f085b41
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-06 06:57:24 -0700

    task 10 added

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,9 @@
 /**
  * add - adds two integer.
  * Return: returns the result of the sum
- */
+ * @suma1: primer parametro
+ * @suma2: segundo parametro
+*/
 int add(int suma1, int suma2)
 {
 return (suma1 + suma2);
