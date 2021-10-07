c27e247e8152ce82c48d32dae798642934603c01
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-07 12:41:14 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit- Entry point
+ * @c: parameter
+ *
+ * _isdigit:if c is a digit
+ * Return: 1 if digit , 0 other
+ */
+int _isdigit(int c)
+{
+if(c >= 48 && c <= 57)
+{    
+return (1);
+}
+else
+{
+return (0);
+}
+}
