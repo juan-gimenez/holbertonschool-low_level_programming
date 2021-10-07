44038f258ef60186b0032e0ef73a2167e832742e
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-07 14:35:25 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints from 0 to 9 (no2-4).
+ * Return: no return.
+ */
+void print_most_numbers(void)
+{
+int ch;
+
+for (ch = 48; ch < 58; ch++)
+{
+if (ch != 6 && ch != 9)
+{
+_putchar(ch);
+}
+_putchar(10);
+}
