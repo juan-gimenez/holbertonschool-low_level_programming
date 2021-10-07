a48a6bdcaa31d4738f7e21f8361cc1ebb8b3d54a
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-07 11:37:56 -0700

    tasks started

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * @c: parameter
+ *
+ * _isupper:if a letter is uppercase
+ * Return: 1 if upercase , 0 other
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c >= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
