43008f731f53842e9fdf04641ca8bea47f1fec24
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-12 11:59:01 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * Return: no return
+ * @s: pointer
+ */
+void print_rev(char *s)
+{
+int i = 99;
+int str[100];
+while (str >= 0)
+{_putchar(str[i]);
+i--; }
+_putchar(10);
+}
