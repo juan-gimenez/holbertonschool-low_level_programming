054f3ee3d552a638ffb5a286e1fb1db5091e61cc
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-12 16:08:32 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other char
+ * Return: zero sucess
+ * @str: string
+ */
+void puts2(char *str)
+{
+int i;
+for(i = 0; i < _strlen(str); i+=2)
+{
+putchar(str[i]);
+}
+}
