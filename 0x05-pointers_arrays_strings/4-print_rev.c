f19ff24147054eca32f5eb3fc517274e12f1c7a0
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-12 17:06:54 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,8 +9,11 @@ void print_rev(char *s)
 {
 int len = 0;
 int i;
-while (s[i++])
-len++;
-for (i = len - 1; i >= 0; i--)
+while (s[i++]){
+len++;}
+for (i = len - 1; i >= 0; i--);
+{
 _putchar(s[i]);
+}
+_putchar(10);
 }
