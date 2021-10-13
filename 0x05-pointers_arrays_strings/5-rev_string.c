ab47eb05fe0fbe2dabb259f1a053f406bad4d981
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-13 08:12:33 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ *
+ **/
+void rev_string(char *s)
+{
+  char buffer[500];
+  int i;
+  int j;
+  for (i = 0; str[i];i++)
+    buffer[i] = str[i]
+      for (i--; i >= 0;i--)
+         str[j-i] = buffer[i]
+	   _putchar(buffer[i]);
+}
