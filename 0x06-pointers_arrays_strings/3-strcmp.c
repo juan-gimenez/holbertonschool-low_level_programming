6f49b71859e147a371425d5f0b86925276e24c82
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-15 10:28:20 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,17 @@
 int _strcmp(char *s1, char *s2)
 {
 while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
-{
-break;
-s1++;
-s2++;
-}
-if (*s1 == *s2)
-{
-return (0); /** if s1 and s2 equal return 0 */
-}
-else
-{
-return (*s1 - *s2);
-}
+  {
+    s1++;
+    s2++;
+    break;
+  }
+ if (*s1 == *s2)
+   {
+     return (0); /** if s1 and s2 equal return 0 */
+   }
+ else
+   {
+     return (*s1 - *s2);
+   }
 }
