df720cd98160ec4828e340a062f5039bf5e03e7e
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-15 09:57:32 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,16 +3,21 @@
 /**
  * _strcat - concatenates two strings
  * Return: dest
- */
+ * @dest: string
+ * @src: string
+*/
 
 char *_strcat(char *dest, char *src)
 {
 int i;
 int j;
-for (i = 0; dest[i] != '\0'; i++);
-for (j = 0; src[j] != '\0'; j++) {
-dest[i+j] = src[j];
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i + j] = src[j];
 }
-dest[i+j] = '\0';
+desti + j] = '\0';
 return (dest);
 }
