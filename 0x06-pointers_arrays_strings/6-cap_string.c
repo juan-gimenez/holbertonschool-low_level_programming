1e8cbd50a356a538e8ab28e56fcce46db55cbdcd
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-15 21:40:22 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ *@a: array
+ *@n: size
+ * Return: no return
+ */
+char *cap_string(char *)
+{
+char str[];
+int i;
+ for (i = 0; str[i] != NULL; i++)
+   if (i == 0)
+     {
+       if((str[i] >= 'a' && str[i] <= 'z'))
+	 str[i]= str[i] - 32;
+       continue;
+     }
+ if (str[i] == ' ')
+   {
+     i++;
+     
