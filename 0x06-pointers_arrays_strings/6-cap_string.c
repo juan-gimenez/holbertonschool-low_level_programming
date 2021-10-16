4127c47f5266f439d082a4a56bdf93104e147a15
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-16 14:58:02 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,13 @@ char *cap_string(char *)
 {
 char str[];
 int i;
- for (i = 0; str[i] != NULL; i++)
-   if (i == 0)
-     {
-       if((str[i] >= 'a' && str[i] <= 'z'))
-	 str[i]= str[i] - 32;
-       continue;
-     }
- if (str[i] == ' ')
-   {
-     i++;
-     
+for (i = 0; str[i] != NULL; i++)
+if (i == 0)
+{
+if((str[i] >= 'a' && str[i] <= 'z'))
+str[i]= str[i] - 32;
+continue;
+}
+if (str[i] == ' ')
+{
+i++;
