fc366e7ecdb7bff5b7aad0d62c8f4b7c973536a9
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-18 07:30:17 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * Return: pointer to memory area s
+ *@dest: dest str
+ *@src: string
+ *@n: first n bytes to copy
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+  char *dst = s;
+  while (n > 0)
+{
+  *deschar *dst = s;
+  while (n > 0)
+    {
+      *dst = (unsigned char) b;
+      dst++;
+      n--;
+    }t = *src;
+      dest++;
+      n--;
+    }
+return (dest);
+}
