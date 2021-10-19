3fd23ada88c23ff50b95d1af0ae685d81c5ff0cb
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-19 09:52:37 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,34 @@
 #include "main.h"
 /**
- * _strspn - gets the length of a prefix substring.
- * Return: number of bytes in the initial segment of s
+ * _strpbrk -  searches a string for any of a set of bytes.
+ * Return: pointer to s that matches
  *@haystack: string
- *@aneedle: second str to find
-*/
+ *@needle: sub string to match
+ */
 char *_strstr(char *haystack, char *needle)
 {
-  int i, j = 0;
-  int nTemp = i;
-  for(i = 0; i<nStrLen-nStrSubLen; i++)
-    {
-      nTemp = i;
-      for(j=0; j<nStrSubLen; j++)
-	{
-
-	  if(str[nTemp]==strSub[j])
-	    {
-	      if(j==nStrSubLen-1)
-		return 1;
-	      nTemp++;
-	    }
-	  else
-	    break;
-	}
-    }
-  return (0);
+int i;
+int j;
+int count = 0;
+ for (i = 0; s[i] != 0; i++)
+   {
+     for (j = 0; accept[j] != 0; j++)
+       {
+	 if (s[i] == accept[j])
+	   {
+	     count++;
+	     s = s + i;
+	     break;
+	   }
+       }
+     if (count == 1)
+       {
+	 break;
+       }
+   }
+ if (count < 1)
+   {
+     s = '\0';
+   }
+ return (s);
 }
