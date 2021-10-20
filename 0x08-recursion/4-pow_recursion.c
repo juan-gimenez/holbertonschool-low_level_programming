e896a4ae97edf65ed6c703d788d1df8d9d0cb31a
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-20 16:42:07 -0700

    task 4 done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,8 +11,6 @@ int _pow_recursion(int x, int y)
 if (y == 0)
 return (1);
 else if (y > 0)
-return (-1);
-else if (y > 0)
 return (x * _pow_recursion(x, y - 1));
 else
 return (1 / _pow_recursion(x, -y));
