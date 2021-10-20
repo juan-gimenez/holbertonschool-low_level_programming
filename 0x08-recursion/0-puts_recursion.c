e7c510d5e03781bb4bfd4e99323d1ee8ce6d191f
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-20 06:39:38 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - returns a string using recursion.
+ * Return: string
+ *@s: given string
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+_putchar(*s);
+_puts_recursion(s + 1);
+_putchar(10);
+}
+}
