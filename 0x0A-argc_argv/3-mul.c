38be15de1fec8439035da154076f4439de0cd756
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-22 10:40:20 -0700

    task 3 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - program that multiplies two numbers
+ *@argc: number of arguments
+ *@argv: strings passed
+ *Return: 0 success
+*/
+int main(int argc, char *argv[])
+{
+if (argc > 3 && argc < 2)
+{
+printf("Error");
+return (1);
+}
+else
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+}
+return (0);
+}
