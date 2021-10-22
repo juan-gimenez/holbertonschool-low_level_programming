501c888826bad176fa5c60b792ff4aa71cdf53da
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-22 07:28:44 -0700

    kans

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  *Return: 0.
  */
 
-int main(int argc, char *argv[])
-{int i = argc;
-printf("%s\n", argv[i] - 1);
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n",argv[0]);
 return (0);
 }
