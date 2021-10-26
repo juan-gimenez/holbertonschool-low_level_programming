aa2caab23a07f4cef22ab5c3d345a6b766fdef97
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-26 09:46:11 -0700

    task 1 done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,25 +3,27 @@
 /**
  *_strdup - strdup function
  *Return: pointer to a new string.
- */
+ *@str: string to copy
+*/
 char *_strdup(char *str)
 {
-  char *nwstr;
-  int i;
-  int j;
-  if (str == NULL)
-    {
-      return ('\0');
-    }
-  for (i = 0; str[i] != 0; i++); /** strlen of str */
-  nwstr = (char *)malloc(sizeof(char) * (i + 1));
-  if (nwstr == NULL)
-    {
-      return (NULL);
-    }
-  for (j = 0; i < j; j++)
-    {
-      str[i] = nwstr[j];
-	}
-  return (nwstr);
+char *nwstr;
+int i;
+int j;
+if (str == NULL)
+{
+return (NULL);
+}
+for (i = 0; str[i] != '\0'; i++)
+; /** strlen of str */
+nwstr = (char *)malloc(sizeof(char) * (i + 1));
+if (nwstr == NULL)
+{
+return (NULL);
+}
+for (j = 0; j <= i; j++)
+{
+nwstr[j] = str[j];
+}
+return (nwstr);
 }
