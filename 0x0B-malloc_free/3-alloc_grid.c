e6c5dcea33498b6e35844c4908fd1d423b2867c5
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-26 16:35:33 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,42 +9,41 @@
  */
 int **alloc_grid(int width, int height)
 {
-  int i,j;
-  int **array;
-  if (width == 0 || width < 0)
-    {
-      return (NULL);
-    }
-  if (height == 0 || height < 0)
-    {
-      return (NULL);
-    }
-  array = malloc(height * sizeof(int *));
-  
-  if (array == NULL)
-    {
-      free(array);
-      return (NULL);
-    }
-  
-  for (i = 0; i < height; i++)
-    {
-      array[i] = malloc(width * sizeof(int));
-      if (array[i] == NULL)
-	{
-	  for(i--; i >= 0; i--)
-	    free(array[i]);
-	  free(array);
-	  return (NULL);
-	}
-    }
-  for (i = 0; i < height; i++)
-    {
-      for (j = 0; j < width; j++)
-	{
-	  array[i][j] = 0;
-	}
-    }
-  
-  return (array);
+int i, j;
+int **array;
+if (width == 0 || width < 0)
+{
+return (NULL);
+}
+if (height == 0 || height < 0)
+{
+return (NULL);
+}
+array = malloc(height * sizeof(int *));
+
+if (array == NULL)
+{
+free(array);
+return (NULL);
+}
+
+for (i = 0; i < height; i++)
+{
+array[i] = malloc(width * sizeof(int));
+if (array[i] == NULL)
+{
+for (i--; i >= 0; i--)
+free(array[i]);
+free(array);
+return (NULL);
+}
+}
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+array[i][j] = 0;
+}
+}
+return (array);
 }
