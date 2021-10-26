78c481385396a3c014e1cddb7a55a80186883cab
Juan Gimenez <juancitogimenez0@gmail.com>
2021-10-26 16:28:45 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
       return (NULL);
     }
   
-  for (i = 0; i < width; i++)
+  for (i = 0; i < height; i++)
     {
       array[i] = malloc(width * sizeof(int));
       if (array[i] == NULL)
