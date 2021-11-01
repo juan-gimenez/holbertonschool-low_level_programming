1bb1ef135e5aa39b4c2b614f6cbe58bad9942324
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-01 11:38:04 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,6 @@ if (d->name != NULL)
 printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)\n");
-if (d->age != '\0')
 printf("Age: %f\n", d->age);
 if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
