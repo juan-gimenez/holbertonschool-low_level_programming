076ac1c7141c0fa1ca25eb0dceea4b51440415c5
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-01 09:03:30 -0700

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Structure of dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of the dog
+ *
+ * Description: structure
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+  /*
+   *@name: dog name
+   *@age: age of the dog
+   *@owner: dog's owner
+   */
+};
+
+#endif
