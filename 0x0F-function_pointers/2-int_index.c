4f03d33e71e993b37a8c62954cb79a6c75a8ebba
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-03 07:55:41 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ *
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+  int a = 0;
+  if (size <= 0)
+    {
+      return (-1);
+    }
+  if (array != 0 && cmp != 0)
+    {
+      while (a < size)
+	{
+	  if (cmp(array[a]))
+	    {
+	      return (a);
+	    }
+	  a++;
+	}
+    }
+    return (-1);  
+}
