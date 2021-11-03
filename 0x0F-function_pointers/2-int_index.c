4008bff87c376f439ad74018dd370dc1fc4239d2
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-03 08:05:27 -0700

    task 2 betty fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,25 +1,28 @@
 #include "function_pointers.h"
 /**
- *
- *
+ * int_index - searches for an integer.
+ *@array: array
+ *@size: size of array
+ *@cmp: compares
+ *Return: -1 if fail
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-  int a = 0;
-  if (size <= 0)
-    {
-      return (-1);
-    }
-  if (array != 0 && cmp != 0)
-    {
-      while (a < size)
-	{
-	  if (cmp(array[a]))
-	    {
-	      return (a);
-	    }
-	  a++;
-	}
-    }
-    return (-1);  
+int a = 0;
+if (size <= 0)
+{
+return (-1);
+}
+if (array != 0 && cmp != 0)
+{
+while (a < size)
+{
+if (cmp(array[a]))
+{
+return (a);
+}
+a++;
+}
+}
+return (-1);
 }
