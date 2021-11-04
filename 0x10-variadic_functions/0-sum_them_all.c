591e497e04276e7a3f2b5d2cb4cabd2bc9ddd88e
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-04 08:54:28 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,19 +7,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
- unsigned int i;
-  int x = 0;
-  va_list ap;
-  va_start(ap, n);
-  
+unsigned int i;
+int x = 0;
+va_list ap;
+va_start(ap, n);
 if (n == 0)
 {
 return (0);
 }
- for (i = 0; i < n; i++)
-   {
-     x += va_arg(ap, unsigned int);
-   }
- va_end(ap);
- return (x);
+for (i = 0; i < n; i++)
+{
+x += va_arg(ap, unsigned int);
+}
+va_end(ap);
+return (x);
 }
