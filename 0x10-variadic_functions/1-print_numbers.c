6d22afeea39e81c79555eb498f3a092451a4bfa7
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-04 09:25:31 -0700

    task 1 done betty fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,29 +9,25 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-  unsigned int i;
-  va_list ap;
-  va_start(ap, n);
-  
-   for (i = 0; i < n; i++)
-    {
-     
-      printf("%d", va_arg(ap, unsigned int));
-      if (i != n - 1)
-	{
-	  if (separator == 0)
-	    {
-	      continue;
-	    }
-	  printf("%s", separator);
-	}
-      else
-	{
-	}
-    }
-   
-    
-  printf("\n");
-  va_end(ap);
+unsigned int i;
+va_list ap;
+va_start(ap, n);
 
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ap, unsigned int));
+if (i != n - 1)
+{
+if (separator == 0)
+{
+continue;
+}
+printf("%s", separator);
+}
+else
+{
+}
+}
+printf("\n");
+va_end(ap);
 }
