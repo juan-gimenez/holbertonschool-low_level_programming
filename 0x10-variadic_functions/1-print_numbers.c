bafd79b566b8333ecd2442bff89d03fe42344090
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-04 09:15:42 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,24 +9,28 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-unsigned int i;
-va_list ap;
-va_start(ap, n);
-for (i = 0; i < n; i++)
-{
-if (separator == 0)
-{
-continue;
-}
-printf("%d", va_arg(ap, unsigned int));
-if (i != n - 1)
-{
-printf("%s", separator);
-}
-else
-{
-}
-}
-printf("\n");
-va_end(ap);
+  unsigned int i;
+  va_list ap;
+  va_start(ap, n);
+  
+   for (i = 0; i < n; i++)
+    {
+      if (separator == 0)
+	{
+	  continue;
+	}
+      printf("%d", va_arg(ap, unsigned int));
+      if (i != n - 1)
+	{
+	  printf("%s", separator);
+	}
+      else
+	{
+	}
+    }
+
+    
+  printf("\n");
+  va_end(ap);
+
 }
