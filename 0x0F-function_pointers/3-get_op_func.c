20776a22bd7babb4f8c215c68b10980dd3b57f97
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-03 17:02:47 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,17 +1,28 @@
-#include "calc.h"
+#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *selects the correct function to perform in main
+ *get_op_func - selects the correct function to perform in main
  *Return:  pointer to the function
  *@s: operator
  *
  */
 int (*get_op_func(char *s))(int, int)
 {
-
-
-
-
-
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (i < 5)
+{
+if (s[0] == ops[i].op[0] && s[0])
+return (ops[i].f);
+i++;
+}
+return (NULL)
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,58 +1,68 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *contain the 5 following functions
- *5 operation functions
+ *3-op_functions - contain the 5 following functions
+ *Return: 0,5 operation functions
  */
 
 /**
- *op_add - addition of 2 ints
+ *op_sub - sum between a & b
  *Return: addition
+ *@a: variable to add
+ *@b: variable to add
  */
 int op_add(int a, int b)
 {
-  return (a + b);
+return (a + b);
 }
+
 /**
- *op_sub - difference between a + b
+ *op_sub - difference between a & b
  *Return: difference
+ *@a: variable to -
+ *@b: variable to +
  */
 int op_sub(int a, int b)
 {
-  return (a - b);
-  
+return (a - b);
 }
 /**
- *op_mul - product between a + b
+ *op_mul - product between a & b
  *Return: product of a and b
+ *@a: variable to mul
+ *@b: variable to mul
  */
 int op_mul(int a, int b)
 {
-  return (a * b);
+return (a * b);
 }
 /**
- *op_div - division between a + b
+ *op_div - division between a and b
  *Return: result of division
+ *@a: variable to divide
+ *@b: variable to divide
  */
 int op_div(int a, int b)
 {
-  if (b == 0)
-    {
-      printf("Error\n");
-      exit (100);
-    }
-  return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
 }
 /**
  *op_mod - remainder of the division of a by b
  *Return: result of division
+ *@a: variable to divide
+ *@b: variable to divide
  */
 int op_mod(int a, int b)
 {
-  if (b == 0)
-    {
-      printf("Error\n");
-      exit (100);
-    }
-  return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
