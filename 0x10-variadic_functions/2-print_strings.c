e04c7b994be9177dfdd36be3d98da48d61f94cff
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-04 19:23:17 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,11 +10,10 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_start(ap, n);
 unsigned int i;
 va_list ap;
  char * str = 0;
-
+ va_start(ap, n);
  if (n > 0)
    {
      for (i = 0; i < n; i++)
