f1f6bb042a38f33fda9a30866f6f78f1cf8be6fa
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-11 12:50:43 -0800

    added lib

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,15 @@
 size_t print_list(const list_t *h)
 {
 unsigned int numnodes = 0;
-while (h)
+if (h)
 {
-if (h->str == NULL)
+if (h->str != NULL)
 {
-printf("[0] (nil)\n");
+printf("[%d] %s\n", h->len, h->str);
 }
 else
 {
-printf("[%d] %s\n", h->len, h->str);
+printf("[0] (nil)\n");
 }
 h = h->next;
 numnodes++;
