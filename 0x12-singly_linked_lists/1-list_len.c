416b3740cfffa0f3e0d422e2f63626793438122b
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-11 16:00:53 -0800

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+ #include "lists.h"
+#include <stdlib.h>
+/**
+ *list_len - number of elements in a linked list_t list.
+ *@h: list
+ *Return: number of nodes
+ */
+size_t list_len(const list_t *h);
+{
+unsigned int numnodes = 0;
+if (h)
+while (h != NULL)
+{
+h = h->next;
+numnodes++;
+}
+return (numnodes);
+}
