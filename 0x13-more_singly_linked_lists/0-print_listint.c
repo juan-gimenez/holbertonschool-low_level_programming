dc83ddcc1a53983b570257068bb063e210741ec3
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-16 13:02:51 -0800

    fixed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,19 +6,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-unsigned int numnodes = 0;
+size_t numnodes = 0;
 const  listint_t *tmp = h;
 
 while (tmp != NULL)
 {
-if (tmp->next == NULL)
-{
 printf("%d\n", tmp->n);
-}
-else
-{
-printf("%d \n", tmp->n);
-}
 tmp = tmp->next;
 numnodes++;
 }
