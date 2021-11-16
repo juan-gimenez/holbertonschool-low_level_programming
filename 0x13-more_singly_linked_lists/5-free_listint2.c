e6f3d8998a9f680c46728125786e22afc95c71a9
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-16 13:15:44 -0800

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,15 +2,16 @@
 /**
  *free_listint2 - frees a listint_t
  *Return: 0
+ *@head: lists
  */
 void free_listint2(listint_t **head)
 {
-  listint_t *temp;
-  while (*head)
-    {
-    temp = (*head)->next;
-    free(*head);
-    *head = temp;
-   }
-  head = NULL;
+listint_t *temp;
+while (*head)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
+*head = NULL;
 }
