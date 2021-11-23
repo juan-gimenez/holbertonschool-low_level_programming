c7f7e8821fd6b791cc25e452b1846dce9abf0df9
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-22 17:07:26 -0800

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+y#include "main.h"
+/**
+ * create_file - creates a file
+ *@filename: file
+ *@text_content: content of new file
+ *Return: 1 if success or -1 if not
+ */
+int create_file(const char *filename, char *text_content)
+{
+int openf, writef, index = 0;
+char *buff;
+if (filename == NULL)
+return (-1);
+if (text_content == NULL)
+{
+openf = open(filename, O_CREAT, 0700);
+if (openf == -1)
+return (-1);
+return (1);
+}
+while (text_content[index] != '\0')
+{
+index++;
+}
+buff = malloc(index * (sizeof(char));
+if (buff == NULL)
+return (-1);
+openf = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0700);
+if (openf == -1)
+return (-1);
+writef = write(openf, text_content, index);
+if (writef == -1)
+return (-1);
+close(openf);
+free(buff);
+return (1); /** succes */
+}
