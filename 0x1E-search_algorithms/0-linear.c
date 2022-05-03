916a5ca1e22fdae3b203baa6c9d8aadf3ad37c2f
juan-gimenez <juancitogimenez0@gmail.com>
2022-05-03 08:38:15 -0700

    fixed gcc error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 {
 size_t x = 0;
 
-while (i < size)
+while (x < size)
 {
 printf("Value checked array[%lu] = [%d]\n", x, array[x]);
 if (array[x] == value)
